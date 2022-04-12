25587d28f96aa10de80912f958affb6bb8e02e8a
Kang Hyung Won <engen701@gmail.com>
2022-04-12 23:00:18 +0900

    feature: add ownership code

---
+++ ownership/target/debug/deps/ownership-a65425269db6792c.d
@@ -0,0 +1,5 @@
+/Users/kanghyungwon/study/rust/ownership/target/debug/deps/ownership-a65425269db6792c: src/main.rs
+
+/Users/kanghyungwon/study/rust/ownership/target/debug/deps/ownership-a65425269db6792c.d: src/main.rs
+
+src/main.rs:
